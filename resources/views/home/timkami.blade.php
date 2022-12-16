c8adcb56b6a2ceff1f6baeb7696f7c7b67ba6e9d
Bayu Yuhartono <bayuyuhartono@gmail.com>
2022-12-16 11:21:16 +0700

    tentang-kami

--- resources/views/home/timkami.blade.php
+++ resources/views/home/timkami.blade.php
@@ -41,6 +41,13 @@
             memberikan sajian berita yang akurat, dan menjadi penyeimbang ditengah
             derasnya arus informasi yang diterima oleh masyarakat.<br>
         </p>
+
+        <h2>Alamat</h2>
+        <p>Medokan asri barat blok i no. 23, surabaya</p>
+
+        <h2>Email</h2>
+        <p><a href="mailto:media@indrajatim.com">media@indrajatim.com</a></p>
+
         {{-- <h2>Tim Kami</h2>
         <p><b>Dewan Redaksi</b></p>
         <p>Fahmi Faqih</p>
